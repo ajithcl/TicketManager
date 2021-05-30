78c526a478901674104e9d5c6a7e415459fe9b0b
ajithcl <clajith@gmail.com>
2021-05-30 20:38:50 +0530

    Object screen.

--- TicketManager/ObjectForm.Designer.cs
+++ TicketManager/ObjectForm.Designer.cs
@@ -32,53 +32,67 @@ namespace TicketManager
             this.tabs = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabUpdate = new System.Windows.Forms.TabPage();
-            this.Search = new System.Windows.Forms.TabPage();
+            this.tabSearch = new System.Windows.Forms.TabPage();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dgvObjects = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTicket = new System.Windows.Forms.TextBox();
+            this.txtObject = new System.Windows.Forms.TextBox();
+            this.cmbActivity = new System.Windows.Forms.ComboBox();
+            this.rtbComments = new System.Windows.Forms.RichTextBox();
             this.tabs.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabUpdate.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvObjects)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabs
             // 
             this.tabs.Controls.Add(this.tabPage1);
             this.tabs.Controls.Add(this.tabUpdate);
-            this.tabs.Controls.Add(this.Search);
+            this.tabs.Controls.Add(this.tabSearch);
             this.tabs.Location = new System.Drawing.Point(-1, 1);
             this.tabs.Name = "tabs";
             this.tabs.SelectedIndex = 0;
-            this.tabs.Size = new System.Drawing.Size(799, 322);
+            this.tabs.Size = new System.Drawing.Size(578, 252);
             this.tabs.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.dgvObjects);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(791, 293);
+            this.tabPage1.Size = new System.Drawing.Size(570, 223);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "View";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabUpdate
             // 
+            this.tabUpdate.Controls.Add(this.panel1);
             this.tabUpdate.Location = new System.Drawing.Point(4, 25);
             this.tabUpdate.Name = "tabUpdate";
             this.tabUpdate.Padding = new System.Windows.Forms.Padding(3);
-            this.tabUpdate.Size = new System.Drawing.Size(791, 293);
+            this.tabUpdate.Size = new System.Drawing.Size(570, 223);
             this.tabUpdate.TabIndex = 1;
             this.tabUpdate.Text = "Update";
             this.tabUpdate.UseVisualStyleBackColor = true;
             // 
-            // Search
+            // tabSearch
             // 
-            this.Search.Location = new System.Drawing.Point(4, 25);
-            this.Search.Name = "Search";
-            this.Search.Padding = new System.Windows.Forms.Padding(3);
-            this.Search.Size = new System.Drawing.Size(791, 293);
-            this.Search.TabIndex = 2;
-            this.Search.Text = "tabSearch";
-            this.Search.UseVisualStyleBackColor = true;
+            this.tabSearch.Location = new System.Drawing.Point(4, 25);
+            this.tabSearch.Name = "tabSearch";
+            this.tabSearch.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSearch.Size = new System.Drawing.Size(570, 223);
+            this.tabSearch.TabIndex = 2;
+            this.tabSearch.Text = "Search";
+            this.tabSearch.UseVisualStyleBackColor = true;
             // 
             // statusStrip1
             // 
@@ -87,7 +101,7 @@ namespace TicketManager
             this.toolStripStatusLabel1});
             this.statusStrip1.Location = new System.Drawing.Point(0, 256);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(640, 26);
+            this.statusStrip1.Size = new System.Drawing.Size(577, 26);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -97,11 +111,94 @@ namespace TicketManager
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
             this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
+            // dgvObjects
+            // 
+            this.dgvObjects.AllowUserToDeleteRows = false;
+            this.dgvObjects.AllowUserToOrderColumns = true;
+            this.dgvObjects.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvObjects.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvObjects.Location = new System.Drawing.Point(6, 6);
+            this.dgvObjects.Name = "dgvObjects";
+            this.dgvObjects.RowHeadersWidth = 51;
+            this.dgvObjects.RowTemplate.Height = 24;
+            this.dgvObjects.Size = new System.Drawing.Size(556, 211);
+            this.dgvObjects.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rtbComments);
+            this.panel1.Controls.Add(this.cmbActivity);
+            this.panel1.Controls.Add(this.txtObject);
+            this.panel1.Controls.Add(this.txtTicket);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 219);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ticket No: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Object Name: ";
+            // 
+            // txtTicket
+            // 
+            this.txtTicket.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTicket.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTicket.Location = new System.Drawing.Point(132, 11);
+            this.txtTicket.Name = "txtTicket";
+            this.txtTicket.Size = new System.Drawing.Size(304, 30);
+            this.txtTicket.TabIndex = 2;
+            // 
+            // txtObject
+            // 
+            this.txtObject.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtObject.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtObject.Location = new System.Drawing.Point(132, 47);
+            this.txtObject.Name = "txtObject";
+            this.txtObject.Size = new System.Drawing.Size(304, 30);
+            this.txtObject.TabIndex = 3;
+            // 
+            // cmbActivity
+            // 
+            this.cmbActivity.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbActivity.FormattingEnabled = true;
+            this.cmbActivity.Location = new System.Drawing.Point(452, 46);
+            this.cmbActivity.Name = "cmbActivity";
+            this.cmbActivity.Size = new System.Drawing.Size(105, 31);
+            this.cmbActivity.TabIndex = 4;
+            // 
+            // rtbComments
+            // 
+            this.rtbComments.Location = new System.Drawing.Point(20, 85);
+            this.rtbComments.Name = "rtbComments";
+            this.rtbComments.Size = new System.Drawing.Size(536, 116);
+            this.rtbComments.TabIndex = 5;
+            this.rtbComments.Text = "";
+            // 
             // ObjectForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 282);
+            this.ClientSize = new System.Drawing.Size(577, 282);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabs);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -111,8 +208,13 @@ namespace TicketManager
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Objects";
             this.tabs.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabUpdate.ResumeLayout(false);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvObjects)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,6 +227,14 @@ namespace TicketManager
         private System.Windows.Forms.TabPage tabUpdate;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
-        private System.Windows.Forms.TabPage Search;
+        private System.Windows.Forms.TabPage tabSearch;
+        private System.Windows.Forms.DataGridView dgvObjects;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTicket;
+        private System.Windows.Forms.TextBox txtObject;
+        private System.Windows.Forms.ComboBox cmbActivity;
+        private System.Windows.Forms.RichTextBox rtbComments;
     }
 }
