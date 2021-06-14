7a6cbc799cef1d1f82f6f435b895ed6fe2f36ae7
clajith <clajith@gmail.com>
2021-06-14 17:55:25 +0530

    Delete button added to Object screen.

--- TicketManager/ObjectForm.Designer.cs
+++ TicketManager/ObjectForm.Designer.cs
@@ -49,6 +49,7 @@ namespace TicketManager
             this.label3 = new System.Windows.Forms.Label();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.tabs.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvObjects)).BeginInit();
@@ -65,10 +66,10 @@ namespace TicketManager
             this.tabs.Controls.Add(this.tabPage1);
             this.tabs.Controls.Add(this.tabUpdate);
             this.tabs.Controls.Add(this.tabSearch);
-            this.tabs.Location = new System.Drawing.Point(-1, 1);
+            this.tabs.Location = new System.Drawing.Point(-1, 0);
             this.tabs.Name = "tabs";
             this.tabs.SelectedIndex = 0;
-            this.tabs.Size = new System.Drawing.Size(578, 252);
+            this.tabs.Size = new System.Drawing.Size(578, 308);
             this.tabs.TabIndex = 0;
             this.tabs.SelectedIndexChanged += new System.EventHandler(this.tabs_SelectedIndexChanged);
             this.tabs.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tabs_MouseClick);
@@ -79,7 +80,7 @@ namespace TicketManager
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(570, 223);
+            this.tabPage1.Size = new System.Drawing.Size(570, 279);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "View";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -94,7 +95,7 @@ namespace TicketManager
             this.dgvObjects.Name = "dgvObjects";
             this.dgvObjects.RowHeadersWidth = 51;
             this.dgvObjects.RowTemplate.Height = 24;
-            this.dgvObjects.Size = new System.Drawing.Size(564, 217);
+            this.dgvObjects.Size = new System.Drawing.Size(564, 273);
             this.dgvObjects.TabIndex = 0;
             this.dgvObjects.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvObjects_CellDoubleClick);
             // 
@@ -104,13 +105,14 @@ namespace TicketManager
             this.tabUpdate.Location = new System.Drawing.Point(4, 25);
             this.tabUpdate.Name = "tabUpdate";
             this.tabUpdate.Padding = new System.Windows.Forms.Padding(3);
-            this.tabUpdate.Size = new System.Drawing.Size(570, 223);
+            this.tabUpdate.Size = new System.Drawing.Size(570, 279);
             this.tabUpdate.TabIndex = 1;
             this.tabUpdate.Text = "Update";
             this.tabUpdate.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btnDelete);
             this.panel1.Controls.Add(this.btnSave);
             this.panel1.Controls.Add(this.rtbComments);
             this.panel1.Controls.Add(this.cmbActivity);
@@ -120,26 +122,26 @@ namespace TicketManager
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(-3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(570, 219);
+            this.panel1.Size = new System.Drawing.Size(570, 273);
             this.panel1.TabIndex = 0;
             // 
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(452, 10);
+            this.btnSave.Location = new System.Drawing.Point(451, 240);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(105, 30);
             this.btnSave.TabIndex = 6;
-            this.btnSave.Text = "SAVE";
+            this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // rtbComments
             // 
             this.rtbComments.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbComments.Location = new System.Drawing.Point(20, 85);
+            this.rtbComments.Location = new System.Drawing.Point(20, 93);
             this.rtbComments.Name = "rtbComments";
-            this.rtbComments.Size = new System.Drawing.Size(536, 116);
+            this.rtbComments.Size = new System.Drawing.Size(536, 141);
             this.rtbComments.TabIndex = 5;
             this.rtbComments.Text = "";
             // 
@@ -147,7 +149,7 @@ namespace TicketManager
             // 
             this.cmbActivity.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbActivity.FormattingEnabled = true;
-            this.cmbActivity.Location = new System.Drawing.Point(452, 46);
+            this.cmbActivity.Location = new System.Drawing.Point(451, 29);
             this.cmbActivity.Name = "cmbActivity";
             this.cmbActivity.Size = new System.Drawing.Size(105, 31);
             this.cmbActivity.TabIndex = 4;
@@ -197,7 +199,7 @@ namespace TicketManager
             this.tabSearch.Location = new System.Drawing.Point(4, 25);
             this.tabSearch.Name = "tabSearch";
             this.tabSearch.Padding = new System.Windows.Forms.Padding(3);
-            this.tabSearch.Size = new System.Drawing.Size(570, 223);
+            this.tabSearch.Size = new System.Drawing.Size(570, 279);
             this.tabSearch.TabIndex = 2;
             this.tabSearch.Text = "Search";
             this.tabSearch.UseVisualStyleBackColor = true;
@@ -209,7 +211,7 @@ namespace TicketManager
             this.dgvSearch.Name = "dgvSearch";
             this.dgvSearch.RowHeadersWidth = 51;
             this.dgvSearch.RowTemplate.Height = 24;
-            this.dgvSearch.Size = new System.Drawing.Size(559, 162);
+            this.dgvSearch.Size = new System.Drawing.Size(559, 219);
             this.dgvSearch.TabIndex = 1;
             // 
             // panel2
@@ -256,7 +258,7 @@ namespace TicketManager
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 256);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 311);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(577, 26);
             this.statusStrip1.TabIndex = 1;
@@ -265,15 +267,26 @@ namespace TicketManager
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
-            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(15, 20);
+            this.toolStripStatusLabel1.Text = "..";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(20, 240);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(105, 30);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // ObjectForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
-            this.ClientSize = new System.Drawing.Size(577, 282);
+            this.ClientSize = new System.Drawing.Size(577, 337);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabs);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -321,5 +334,6 @@ namespace TicketManager
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridView dgvSearch;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
